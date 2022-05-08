ce10d51772f2ca9171c0362ab4223d0327a84e6b
Anne Barois <barois.anne@orange.fr>
2022-05-08 10:25:19 +0200

    Add front pop up and check dyslexia

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
-let color = '#3aa757';
+//Initialize new Font Family
+let fontFamily = 'Helvetica, Arial, Tahoma, Lucida, Verdana, sans-serif';
+
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  chrome.storage.sync.set({ fontFamily });
+  console.log('Pouette');
 });
+//End font family
--- popup.js
+++ popup.js
@@ -1,17 +1,29 @@
+// Initialize check button of dislexia
+let changeFont = document.getElementById("dislexia");
 
-changeColor.addEventListener("click", async () => {
+chrome.storage.sync.get("fontFamily", ({ fontFamily }) => {
+  changeFont.style.fontFamily = fontFamily;
+});
+
+
+changeFont.addEventListener("change", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: setPageFontFamily,
   });
 });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+function setPageFontFamily() {
+  chrome.storage.sync.get("fontFamily", ({ fontFamily }) => {
+    //let title = document.getElementsByTagName("h1");
+    console.log(title);
+    document.body.style.fontFamily = fontFamily;
+    //document.title.style.fontFamily = fontFamily;
   });
 }
+
+// End of dislexia checked
