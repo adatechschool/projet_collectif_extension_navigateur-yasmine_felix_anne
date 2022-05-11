e890e43a2c332fd2aebbb12f82c84db916d3657d
Anne Barois <barois.anne@orange.fr>
2022-05-11 16:40:00 +0200

    Add timer function - working

--- background.js
+++ background.js
@@ -13,3 +13,23 @@ let fontFamily = 'Open-Dyslexic, Andika, Helvetica, Arial, Tahoma, Lucida, Verda
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ fontFamily });
 });
+
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.action.setBadgeText({ text: '' });
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: 'stay_hydrated.png',
+    title: 'Time to Hydrate',
+    message: 'Everyday I\'m Guzzlin\'!',
+    buttons: [
+      { title: 'Keep it Flowing.' }
+    ],
+    priority: 0
+  });
+});
+
+chrome.notifications.onButtonClicked.addListener(async () => {
+  const item = await chrome.storage.sync.get(['minutes']);
+  chrome.action.setBadgeText({ text: 'ON' });
+  chrome.alarms.create({ delayInMinutes: item.minutes });
+});
